d689215cc779ccd0852d165f9cbd7558b0ef9017
snowlizard <illegal_burrito@live.com>
2023-10-29 19:35:07 -0700

    init appt form

---
+++ src/main/java/c195/schedulingapp/Controllers/AppointmentForm.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package c195.schedulingapp.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author mrjack
+ */
+public class AppointmentForm implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void onSave(){
+    }
+    
+    public void onCancel(){
+    }
+}
